5e35948ff7874ec3d8cd891a0874a39a284aea3a
Einar <einardavidvillarroel@gmail.com>
2021-06-08 14:04:42 -0400

    Nuevo

---
+++ RAFiltros/Assets/Scripts/FaceController.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR.ARFoundation;
+using System;
+using UnityEngine.EventSystems;
+[RequireComponent(typeof(ARFaceManager))]
+
+public class FaceController : MonoBehaviour
+{
+    [SerializeField]
+    private Button faceTrackingToggle;
+
+    [SerializeField]
+    private Button btnAdelante; 
+
+    [SerializeField]
+    private Button btnAtras;
+
+    private ARFaceManager arFaceManager;
+    
+    private bool faceTrackingOn = true;
+
+    private int swapCounter = 0;
+
+    [SerializeField]
+    public FaceMaterial[] materials;
+
+    void Awake() 
+    {
+        swapCounter = materials.Length - 1;
+        arFaceManager = GetComponent<ARFaceManager>();
+        
+        faceTrackingToggle.onClick.AddListener(ToggleTrackingFaces);
+        btnAdelante.onClick.AddListener(Adelante);
+        btnAtras.onClick.AddListener(Atras);
+
+        arFaceManager.facePrefab.GetComponent<MeshRenderer>().material = materials[0].Material;
+    }
+
+
+    void Adelante()  
+    { 
+        swapCounter = swapCounter == materials.Length - 1 ? 0 : swapCounter + 1;
+        
+        foreach(ARFace face in arFaceManager.trackables)
+        {
+            face.GetComponent<MeshRenderer>().material = materials[swapCounter].Material;
+        }
+
+        btnAdelante.GetComponentInChildren<Text>().text = $"({materials[swapCounter].Name})";
+    }
+        void Atras() 
+    { 
+        swapCounter = swapCounter < 0 ? materials.Length - 1 : swapCounter - 1;
+        
+        foreach(ARFace face in arFaceManager.trackables)
+        {
+            face.GetComponent<MeshRenderer>().material = materials[swapCounter].Material;
+        }
+
+        btnAtras.GetComponentInChildren<Text>().text = $"({materials[swapCounter].Name})";
+        //btnAtras.GetComponentInChildren<Text>().text = $"Face Material ({materials[swapCounter].Name})";
+    }
+
+    void ToggleTrackingFaces() 
+    { 
+        faceTrackingOn = !faceTrackingOn;
+
+        foreach(ARFace face in arFaceManager.trackables)
+        {
+            face.enabled = faceTrackingOn;
+        }
+        
+        faceTrackingToggle.GetComponentInChildren<Text>().text = $"Face Tracking {(arFaceManager.enabled ? "Off" : "On" )}";
+    }
+
+}
+
+[System.Serializable]
+public class FaceMaterial
+{
+    public Material Material;
+
+    public string Name;
+}
---
+++ RAFiltros/Assets/Scripts/TogleFace.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR.ARFoundation;
+using System;
+using UnityEngine.EventSystems;
+
+public class TogleFace : MonoBehaviour
+{
+    public ARFaceManager arFaceManager;
+
+    public GameObject[] faces;
+
+    public Button[] botones;
+
+    private float timer;
+
+    [SerializeField]
+    private float frequency = 1.0f;
+
+    [SerializeField]
+    private float maxUntilSpawn = 1.0f;
+
+    private int index = -1;
+
+    void Start()
+    {
+        arFaceManager = gameObject.AddComponent<ARFaceManager>();   
+        //arFaceManager.facePrefab = faces[index];
+        for (int i = 0; i < botones.Length; i++)
+        {
+            botones[i].onClick.AddListener(CambiarCara);
+        }
+    }
+    void Update()
+    {
+
+    }
+    void CambiarCara()
+    {
+        index++;
+        if(index>=botones.Length)
+        {
+            index=0;
+        }
+        //index = Convert.ToInt32(EventSystem.current.currentSelectedGameObject.name);
+        //arFaceManager.facePrefab = new GameObject();
+        arFaceManager.facePrefab = faces[index];
+        Debug.Log(index);
+    }
+}
